981d77129e724a8722556e50c7781bf4df4f44f6
changshu.qd <changshu.qd@04c7a32b-c345-0410-8ced-997fcb3cb136>
2008-02-10 12:31:11 +0000

    New classes used to specify the Information of Projects and Dictionaries

---
+++ ws-fuzzer/src/datamodel/WSFDictionaryInfo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package datamodel;
+
+/**
+ *
+ * @author chang
+ */
+public class WSFDictionaryInfo {
+
+}
